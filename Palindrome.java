f27dafe3f64eccd8d5f7d573c4f9d09242820cb9
kavyanjali5166 <152371632+kavyanjali5166@users.noreply.github.com>
2024-08-17 11:35:03 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+public class Palindrome
+{
+	public static void main(String args[])
+	{
+	   int r,s=0,temp,x;
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter +ve integer to check the given number is Palindrome or not");
+	   x=sc.nextInt();
+	   temp=x;
+	   while(x>0)
+		   
